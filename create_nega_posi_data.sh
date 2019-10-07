a310a6c7753cfba1db6362290e6529b4f1c268b0
komorihi <7703110+komorihi@users.noreply.github.com>
2019-10-07 19:08:29 +0900

    Update data creation script

--- create_nega_posi_data.sh
+++ create_nega_posi_data.sh
@@ -1,9 +1,59 @@
-#!/bin/bash
+#! /bin/bash
+function usage() {
+cat <<_EOT_
+Usage:
+  bash create_nega_posi_data.sh [-d dataset name] [-p positives] [-n negatives] [-r test size rate]
+
+Options:
+  -d --dataset   dataset name.
+  -p --positive   positive data directory.
+  -n --negative   negative data directory.
+  -r --test-size   percentage of test data size.(not sample number.) 
+_EOT_
+return 0
+}
+
 DIR="np"
 POS="data/$DIR/positives"
 NEG="data/$DIR/negatives"
 TEST_RATE=10
 PROC=true
+while getopts "dnpr:h-:" OPT
+do
+    case $OPT in
+        -)
+            case "${OPTARG}" in
+                positive)  POS=$OPTARG
+                    ;;
+                negative)  NEG=$OPTARG
+                    ;;
+                dataset)  DIR=$OPTARG
+                    ;;
+                test-size)  TEST_RATE=$OPTARG
+                    ;;
+                help)
+                    usage
+                    PROC=false
+                    ;;
+            esac
+            ;;
+        p)  POS=$OPTARG
+            ;;
+        n)  NEG=$OPTARG
+            ;;
+        d)  DIR=$OPTARG
+            ;;
+        r)  TEST_RATE=$OPTARG
+            ;;
+        h)  usage
+            PROC=false
+            ;;
+        \?) usage
+            PROC=false
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 cp /dev/null "data/corpus/$DIR.txt"
 cp /dev/null "data/$DIR.txt"
@@ -35,4 +85,4 @@ if $PROC; then
     i=$((i+1))
   done
 fi
-return 0
+unset DIR POS NEG TEST_RATE PROC OPT OPTARG OPTIND
